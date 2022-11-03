4222dddf2cbe949a332d0aa758970703de5c6a32
John Cabuguason <jcalc30@gmail.com>
2022-11-03 15:35:36 -0400

    initial login ui css enhancements

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import { BsPersonCircle } from 'react-icons/bs'
-import { FaLock } from 'react-icons/fa'
+import { InputText } from 'primereact/inputtext'
+import { Button } from 'primereact/button'
+import 'primereact/resources/themes/lara-light-indigo/theme.css'
 import '../../Stylings/login.css'
 import { Link, useNavigate } from 'react-router-dom'
 
@@ -30,37 +32,34 @@ function Login (props) {
   }
   return (
     <div className='login-background'>
-      <div className="login-container">
-        <BsPersonCircle size={80} style={{ marginBottom: '30px', marginTop: '40px' }}/>
-
-        <p style={{ marginBottom: '10px', marginRight: '140px' }}>Username: </p>
+      <div className="login-form-container">
+        <BsPersonCircle size={80} className="user-profile-icon"/>
 
         <div className="field-container">
-          <BsPersonCircle size={20}/>
-          <input type='text'
-          value={userName}
-          onChange={(event) => setUserName(event.target.value)}
-          name='username'
-          placeholder='Username'/>
+          <label htmlFor="password">Username</label>
+          <div className="p-inputgroup">
+            <span className="p-inputgroup-addon">
+                <i className="pi pi-user"></i>
+            </span>
+            <InputText value={userName} placeholder='Username' name='username' onChange={(event) => setUserName(event.target.value)} />
+          </div>
         </div>
-
-        <span style={{ marginBottom: '10px', marginRight: '140px' }}>Password: </span>
 
         <div className='field-container'>
-          <FaLock size={20} />
-          <input type='password'
-          value={password}
-          onChange={(event) => setPassword(event.target.value)}
-          onKeyPress={onSearch}
-          name='password'
-          placeholder="Password"/>
+          <label htmlFor="password">Password</label>
+          <div className="p-inputgroup">
+            <span className="p-inputgroup-addon">
+                <i className="pi pi-lock"></i>
+            </span>
+            <InputText type='password' onKeyPress={onSearch} value={password} placeholder='Password' name='password' onChange={(event) => setPassword(event.target.value)}/>
+          </div>
         </div>
-        <div>
-          {!isLoading
-            ? <Link className="login-signin" to="/signin" >Sign in</Link>
-            : <img style={{ width: '60%', height: '20%' }} src={require('../../Images/loading.gif')} alt="loading-gif" />
-            }
+        <div className="actions-container">
+          <Button label="Log In" loading = {isLoading} />
 
+          <Link className="login-signin" to="/signin">
+              <Button label="Sign In" className='p-button-outlined' loading = {isLoading} />
+          </Link>
         </div>
       </div>
     </div>
