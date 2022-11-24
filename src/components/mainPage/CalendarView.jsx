4521e485b0485186edfe4855572a0a5098f950d3
Shant Balolian <shantpaloulian@gmail.com>
2022-11-24 13:59:33 -0500

    Calendar changes

--- src/components/mainPage/CalendarView.jsx
+++ src/components/mainPage/CalendarView.jsx
@@ -5,7 +5,7 @@ import format from "date-fns/format";
 import getDay from "date-fns/getDay";
 import parse from "date-fns/parse";
 import startOfWeek from "date-fns/startOfWeek";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Calendar, dateFnsLocalizer } from "react-big-calendar";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import DatePicker from "react-datepicker";
@@ -23,26 +23,34 @@ const localizer = dateFnsLocalizer({
   locales,
 });
 
-const events = [
-  {
-      title: "Big Meeting",
-      allDay: true,
-      start: new Date(2022, 6, 0),
-      end: new Date(2022, 6, 0),
-  },
-  {
-      title: "Vacation",
-      start: new Date(2022, 6, 7),
-      end: new Date(2022, 6, 10),
-  },
-  {
-      title: "Conference",
-      start: new Date(2022, 6, 20),
-      end: new Date(2022, 6, 23),
-  },
-];
+
 
 function CalendarView(props){
+  const {tasks, setTasks} = props;
+
+  
+
+  const fetchTasks = async() => {
+    const tasksResp = await fetch('http://localhost:5000/api/tasks');
+    return tasksResp;
+  }
+
+  useEffect(() => {
+    console.log("in use Effect")
+    fetchTasks()
+    .then(resp => resp.json())
+    .then(data => setTasks(data))
+    .catch(err => console.log(err))
+  }, [])
+
+
+
+  const events = tasks.map(({created_on, schedule_date, ...item}) => {
+    return ({start: new Date(Date.parse(created_on)),
+             end: new Date(Date.parse(created_on)),
+             ...item})
+            })
+
     return(
         <div>
         <div className='myDay-header-Container'>
@@ -51,7 +59,7 @@ function CalendarView(props){
           <span className='pi pi-ellipsis-h'></span>
         </div>
         <Calendar localizer={localizer} events={events} 
-        startAccessor="start" endAccessor="end" style={{height: 500, margin:"50px"}} />
+        startAccessor="start" endAccessor="end" titleAccessor="name" allDayAccessor="is_completed" style={{height: "600px", margin:"50px", width:"1000px"}} />
       </div>    
       )
 }
